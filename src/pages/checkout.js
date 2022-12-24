b0f188fc7f40db7683b183a83bdecb5094ae8b91
William Robersone <wmrobersone@yahoo.com>
2022-12-24 14:16:47 -0600

    add + remove buttons

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -2,11 +2,15 @@ import React from "react";
 import Header from "../components/Header";
 import Image from "next/image";
 import { useSelector } from "react-redux";
-import { selectItems } from "../slices/basketSlice";
+import { selectItems, selectTotal } from "../slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
+import Currency from "react-currency-formatter";
+import { useSession } from "next-auth/react";
 
 function Checkout() {
   const items = useSelector(selectItems);
+  const total = useSelector(selectTotal);
+  const { data: session } = useSession();
 
   return (
     <div className="bg-gray-100">
@@ -44,6 +48,26 @@ function Checkout() {
           </div>
         </div>
         {/* Total */}
+        <div className="flex flex-col bg-white p-10 shadow-md">
+          {items.length > 0 && (
+            <>
+              <h2>
+                Subtotal ({items.length} items):{" "}
+                <span className="font-bold ">
+                  <Currency quantity={total} currency="USD" />
+                </span>
+              </h2>
+              <button
+                className={`button mt-2 ${
+                  !session &&
+                  "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
+                }`}
+              >
+                {!session ? "Sign In to Checkout" : "Proceed to Checkout"}
+              </button>
+            </>
+          )}
+        </div>
       </main>
     </div>
   );
